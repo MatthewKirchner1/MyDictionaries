1f28e6b63c90099bdf10f65fec42acdcb398da74
Matthew Kirchner <Matthew_Kirchner1@baylor.edu>
2021-02-01 14:46:24 -0600

    initial commit

---
+++ dictionary start file.py
@@ -0,0 +1,113 @@
+phonebook = {"Chris": "555-111", 
+            "Katie": "555−2222",
+            "Joanne": "555-333"}
+
+"""
+print()
+print("*****  start section 1 - print dictionary ********")
+print()
+
+print(phonebook)
+print(len(phonebook))
+
+mydictionary = dict(m=8,n=9)
+print(mydictionary)
+
+print()
+print("*****  end section 1 ********")
+print()
+
+print()
+print("*****  start section 2 - search dictionary ********")
+print()
+
+print(phonebook['Chris'])
+
+name = 'chris'
+
+if name in phonebook:
+    print(phonebook[name])
+else:
+    print('Not Found')
+
+print()
+print("*****  end section 2 ********")
+print()
+
+
+print()
+print("*****  start section 3 - edit/append dictionary ********")
+print()
+
+phonebook['Chris'] = '555-444'
+
+phonebook['Joe'] = '555-0123'
+
+print(phonebook)
+
+print()
+print("*****  end section 3 ********")
+print()
+
+
+
+print()
+print("*****  start section 4 - delete/remove from dictionary ********")
+print()
+
+del phonebook['Chris']
+
+print(phonebook)
+
+print()
+print("*****  end section 4 ********")
+print()
+
+
+
+print()
+print("*****  start section 5 - iterate through keys ********")
+print()
+
+for k in phonebook:
+    print(k)
+    print(phonebook[k])
+
+print()
+print("*****  end section 5 ********")
+print()
+
+
+
+print()
+print("*****  start section 6 - iterate through values  ********")
+print()
+
+
+print()
+print("*****  end section 6 ********")
+print()
+
+"""
+
+print()
+print("*****  start section 7 - iterate through both key and value pair********")
+print()
+
+for k,v in phonebook.items():
+    print(k, v)
+
+print()
+print("*****  end section 7 ********")
+print()
+
+''''
+
+print()
+print("*****  start section 8 - using random and converting to list ********")
+print()
+
+
+print()
+print("*****  end section 8 ********")
+print()
