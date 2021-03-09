c56e8ee1f739016a033b5798461835dcf75feac1
Matthew Kirchner <Matthew_Kirchner1@baylor.edu>
2021-03-09 13:47:56 -0600

    midterm exercise 1

---
+++ Dictionaries_Midterm.py
@@ -0,0 +1,77 @@
+####Part 1
+
+# codes = {'A':'%’, 'a':‘9’, 'B':‘@’, 'b':'--', 'C':'&', 'c':'1', 'D':'µ', 'd':'3'}
+codes = {
+    "A": "%",
+    "a": "9",
+    "B": "@",
+    "b": "--",
+    "C": "&",
+    "c": "1",
+    "D": "µ",
+    "d": "3",
+    "E": "4",
+    "e": "|",
+    "F": "^",
+    "f": "6",
+    "G": "Ô",
+    "g": "{",
+    "H": "ø",
+    "h": "~",
+    "I": ">",
+    "i": "Ç",
+    "J": "-",
+    "j": "0",
+    "K": ";",
+    "k": "¡",
+    "L": "{",
+    "l": "=",
+    "M": ")",
+    "m": "*",
+    "N": "<",
+    "n": "?",
+    "O": "5",
+    "o": "-",
+    "P": "&",
+    "p": "[",
+    "Q": "+",
+    "q": "_",
+    "R": "ß",
+    "r": "‡",
+    "S": "¹",
+    "s": "ê",
+    "T": "ì",
+    "t": "ò",
+    "U": "¿",
+    "u": "ã",
+    "V": "ï",
+    "v": "≠",
+    "W": "Ý",
+    "w": "²",
+    "X": "¦",
+    "x": "¯",
+    "Y": "ù",
+    "y": "ž",
+    "Z": ",",
+    "z": "µ",
+    " ": " ",
+    ".": ".",
+    ":": ":",
+}
+
+
+infile = open("info_security.txt", "r")
+outfile = open("info_security_encrypted.txt", "w")
+
+infile_str = infile.read()
+outfile_str = " "
+
+print(infile_str)
+
+for element in infile_str:
+    new_char = codes[element]
+    outfile_str += new_char
+
+print(outfile_str)
+
+outfile.write(outfile_str)
